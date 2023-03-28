1a9d5306e24029a6c2a432707f55ede87389eadc
keannu125 <noehbernasol0@gmail.com>
2023-03-28 20:47:41 +0800

    Changed relationship between professor and subjects to be many-to-many

--- enrollment/subjects/models.py
+++ enrollment/subjects/models.py
@@ -8,8 +8,16 @@ class Subject(models.Model):
     subject_name = models.CharField(max_length=40)
     description = models.CharField(max_length=200)
     course_id = models.CharField(max_length=20)
-    professor_assigned = models.ForeignKey(Professor, null=True, on_delete=models.SET_NULL)
     date_added = models.DateTimeField(default=now, editable=False)
+    date_updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+    professor_assigned = models.ManyToManyField(
+        'professor.Professor', through='subjects.SubjectProfessor')
 
     def __str__(self):
         return self.subject_name
+
+
+class SubjectProfessor(models.Model):
+    subject = models.ForeignKey('subjects.Subject', on_delete=models.CASCADE)
+    professor_assigned = models.ForeignKey(
+        'professor.Professor', on_delete=models.SET_NULL, null=True)
--- enrollment/subjects/serializers.py
+++ enrollment/subjects/serializers.py
@@ -1,16 +1,21 @@
 from rest_framework import serializers
 from .models import Subject
 from professor.models import Professor
+from professor.serializers import ProfessorSerializer
 
 
 class SubjectSerializer(serializers.HyperlinkedModelSerializer):
     date_added = serializers.DateTimeField(
         format="%d-%m-%Y %I:%M%p", read_only=True)
-    
-    professor_assigned = serializers.PrimaryKeyRelatedField(queryset=Professor.objects.all())
+
+    # professor_assigned = serializers.PrimaryKeyRelatedField(
+    #    queryset=Professor.objects.all(), many=True)
+
+    professor_assigned = serializers.SlugRelatedField(
+        queryset=Professor.objects.all(), many=True, slug_field='first_name', allow_null=True)
 
     class Meta:
         model = Subject
         fields = ('id', 'subject_name',
-                   'description','course_id','professor_assigned','date_added')
+                  'description', 'course_id', 'professor_assigned', 'date_added')
         read_only_fields = ('id', 'date_added')
