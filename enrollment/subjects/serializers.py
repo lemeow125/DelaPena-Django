d0612514097386ba24d235d8e060d00ba838688c
keannu125 <noehbernasol0@gmail.com>
2023-03-28 20:48:15 +0800

    Cleaned up code

--- enrollment/subjects/serializers.py
+++ enrollment/subjects/serializers.py
@@ -1,15 +1,11 @@
 from rest_framework import serializers
 from .models import Subject
 from professor.models import Professor
-from professor.serializers import ProfessorSerializer
 
 
 class SubjectSerializer(serializers.HyperlinkedModelSerializer):
     date_added = serializers.DateTimeField(
         format="%d-%m-%Y %I:%M%p", read_only=True)
-
-    # professor_assigned = serializers.PrimaryKeyRelatedField(
-    #    queryset=Professor.objects.all(), many=True)
 
     professor_assigned = serializers.SlugRelatedField(
         queryset=Professor.objects.all(), many=True, slug_field='first_name', allow_null=True)
